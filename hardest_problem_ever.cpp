e6ff5fd6a75e0e3e84e44e1d9784c9d441726d9e
RA-Snow <53481055+RA-Snow@users.noreply.github.com>
2022-10-15 22:47:14 +0530

    third commit

---
+++ hardest_problem_ever.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+long long gcd(long long a, long long b)
+
+{
+
+    if (a == 0)
+
+        return b;
+
+    return gcd(b % a, a);
+
+}
+
+int main() {
+    // your code goes here
+    int T;
+    cin>>T;
+    while(T--){
+	int n;
+	cin>>n;
+	vector<long long> v(n);
+	for(int i=0;i<n;i++){
+	    cin>>v[i];
+	}
+	long long ans = v[0];
+	for(int i=1;i<n;i++)
+	    ans = gcd(ans,v[i]);
+	if(ans==1) cout<<1<<endl;
+	else cout<<0<<endl;
+    }
+}
